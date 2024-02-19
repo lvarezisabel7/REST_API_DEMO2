53edd032c96c670834a54389173ce6a0f138c7d2
lvarezisabel7 <maisabel2000@gmail.com>
2024-02-19 12:08:31 +0100

    Add productoRepository

---
+++ src/main/java/com/example/repository/ProductoRepository.java
@@ -0,0 +1,13 @@
+package com.example.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.entities.Producto;
+
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+   
+    List<Producto> findProductosByPresentacionesId(Integer presentacionId);
+    
+}
